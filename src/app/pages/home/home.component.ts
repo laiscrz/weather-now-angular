f027e28a061c6c99d6a13ede46c81fe19f894234
laiscrz <lais.albino2003@gmail.com>
2024-12-12 19:28:31 -0300

    feat: updated on page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../../components/header/header.component'
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
 
