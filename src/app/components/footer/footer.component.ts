969665796d484677ecd38b3c6041534631fc37f4
laiscrz <lais.albino2003@gmail.com>
2024-12-12 18:35:37 -0300

    feat: added Footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'weather-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  imports: [ MatIconModule],
+})
+export class  FooterComponent {
+
+}
